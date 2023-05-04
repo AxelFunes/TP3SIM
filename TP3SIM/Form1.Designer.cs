d51ad6a57261757f142138fd69381d28b8f448e8
Axel Funes <chakyfunes@gmail.com>
2023-05-04 19:04:05 -0300

    Agregar archivos de proyecto.

---
+++ TP3SIM/Form1.Designer.cs
@@ -0,0 +1,402 @@
+﻿namespace TP3SIM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgv_simulaciones = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_hasta = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_desde = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_simulacion = new System.Windows.Forms.TextBox();
+            this.btn_simular = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column17 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column18 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column19 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column20 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column21 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column22 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column23 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column24 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column25 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column26 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column27 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_simulaciones)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_simulaciones
+            // 
+            this.dgv_simulaciones.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv_simulaciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_simulaciones.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column13,
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12,
+            this.Column14,
+            this.Column15,
+            this.Column16,
+            this.Column17,
+            this.Column18,
+            this.Column19,
+            this.Column20,
+            this.Column21,
+            this.Column22,
+            this.Column23,
+            this.Column24,
+            this.Column25,
+            this.Column26,
+            this.Column27});
+            this.dgv_simulaciones.Location = new System.Drawing.Point(26, 186);
+            this.dgv_simulaciones.Name = "dgv_simulaciones";
+            this.dgv_simulaciones.RowHeadersWidth = 51;
+            this.dgv_simulaciones.Size = new System.Drawing.Size(991, 257);
+            this.dgv_simulaciones.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(416, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Hasta";
+            // 
+            // txt_hasta
+            // 
+            this.txt_hasta.Enabled = false;
+            this.txt_hasta.Location = new System.Drawing.Point(457, 90);
+            this.txt_hasta.Name = "txt_hasta";
+            this.txt_hasta.Size = new System.Drawing.Size(100, 20);
+            this.txt_hasta.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Desde";
+            // 
+            // txt_desde
+            // 
+            this.txt_desde.Location = new System.Drawing.Point(302, 90);
+            this.txt_desde.Name = "txt_desde";
+            this.txt_desde.Size = new System.Drawing.Size(100, 20);
+            this.txt_desde.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Cantidad de Semanas";
+            // 
+            // txt_simulacion
+            // 
+            this.txt_simulacion.Location = new System.Drawing.Point(131, 90);
+            this.txt_simulacion.Name = "txt_simulacion";
+            this.txt_simulacion.Size = new System.Drawing.Size(100, 20);
+            this.txt_simulacion.TabIndex = 7;
+            // 
+            // btn_simular
+            // 
+            this.btn_simular.Location = new System.Drawing.Point(51, 148);
+            this.btn_simular.Name = "btn_simular";
+            this.btn_simular.Size = new System.Drawing.Size(75, 23);
+            this.btn_simular.TabIndex = 10;
+            this.btn_simular.Text = "Simular";
+            this.btn_simular.UseVisualStyleBackColor = true;
+            this.btn_simular.Click += new System.EventHandler(this.btn_simular_Click);
+            // 
+            // Column1
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column1.HeaderText = "Semanas";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.White;
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column2.HeaderText = "Rnd Demanda";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Column3.HeaderText = "Demanda";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
+            this.Column4.DefaultCellStyle = dataGridViewCellStyle4;
+            this.Column4.HeaderText = "Stock Inicial";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.White;
+            this.Column5.DefaultCellStyle = dataGridViewCellStyle5;
+            this.Column5.HeaderText = "Rnd Falla";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // Column6
+            // 
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
+            this.Column6.DefaultCellStyle = dataGridViewCellStyle6;
+            this.Column6.HeaderText = "Pedido 1 fallado?";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 125;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Rnd Falla";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 125;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Pedido 2 fallado?";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 125;
+            // 
+            // Column13
+            // 
+            this.Column13.HeaderText = "Rnd Falla";
+            this.Column13.Name = "Column13";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Pedido 3 fallado?";
+            this.Column9.MinimumWidth = 6;
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 125;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "Rnd Falla";
+            this.Column10.MinimumWidth = 6;
+            this.Column10.Name = "Column10";
+            this.Column10.Width = 125;
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "Pedido 4 fallado?";
+            this.Column11.Name = "Column11";
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "Rnd Falla";
+            this.Column12.Name = "Column12";
+            // 
+            // Column14
+            // 
+            this.Column14.HeaderText = "Pedido 5 fallado?";
+            this.Column14.Name = "Column14";
+            // 
+            // Column15
+            // 
+            this.Column15.HeaderText = "Rnd Falla";
+            this.Column15.Name = "Column15";
+            // 
+            // Column16
+            // 
+            this.Column16.HeaderText = "Pedido 6 fallado?";
+            this.Column16.Name = "Column16";
+            // 
+            // Column17
+            // 
+            this.Column17.HeaderText = "Cantidad Falladas";
+            this.Column17.Name = "Column17";
+            // 
+            // Column18
+            // 
+            this.Column18.HeaderText = "Stock Final";
+            this.Column18.Name = "Column18";
+            // 
+            // Column19
+            // 
+            this.Column19.HeaderText = "Pide? SI/NO";
+            this.Column19.Name = "Column19";
+            // 
+            // Column20
+            // 
+            this.Column20.HeaderText = "Rnd Demora";
+            this.Column20.Name = "Column20";
+            // 
+            // Column21
+            // 
+            this.Column21.HeaderText = "Cantidad Demora";
+            this.Column21.Name = "Column21";
+            // 
+            // Column22
+            // 
+            this.Column22.HeaderText = "Llegada";
+            this.Column22.Name = "Column22";
+            // 
+            // Column23
+            // 
+            this.Column23.HeaderText = "Costo Tenencia";
+            this.Column23.Name = "Column23";
+            // 
+            // Column24
+            // 
+            this.Column24.HeaderText = "Costo Pedido";
+            this.Column24.Name = "Column24";
+            // 
+            // Column25
+            // 
+            this.Column25.HeaderText = "Costo Agotamiento";
+            this.Column25.Name = "Column25";
+            // 
+            // Column26
+            // 
+            this.Column26.HeaderText = "Costo Total";
+            this.Column26.Name = "Column26";
+            // 
+            // Column27
+            // 
+            this.Column27.HeaderText = "Costo Acumulado";
+            this.Column27.Name = "Column27";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1186, 663);
+            this.Controls.Add(this.dgv_simulaciones);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_hasta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_desde);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_simulacion);
+            this.Controls.Add(this.btn_simular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_simulaciones)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_simulaciones;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_hasta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_desde;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_simulacion;
+        private System.Windows.Forms.Button btn_simular;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column16;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column17;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column18;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column19;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column20;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column21;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column22;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column23;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column24;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column25;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column26;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column27;
+    }
+}
+
---
+++ TP3SIM/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP3SIM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int simulaciones = 0;
+        int desde = 0;
+        int hasta = 0;
+
+        Random rnd = new Random();
+        //Variables para el procedimiento de simulaciones
+        int semanas = 0;
+
+        double random_demanda;
+        int demanda;
+        double random_falla;
+        
+        int stock_Inicial = 7;
+        int stock_Final;
+        string fallada;
+        string pide;
+        double random_demora;
+        int demora;
+        int llegada;
+        int costoTenencia;
+        int costoPedido;
+        int costoAgotamiento;
+        int costoTotal;
+        int costoAcumulado;
+
+        // PARA ABAJO ES EL TP ANTERIOR
+        int pedidoNeto;
+
+        int stockReal;
+        int stockRemanenteAnterior;
+        int stockRemanenteActual;
+
+        // variables para costo
+        int costoMantenimiento;
+        //int costoPedido;
+        int costoSobrepaso;
+        //int costoTotal;
+
+        // variables adicionales
+        int excedenteStock;
+        int porcentajeCosto;
+        int porcentajeMinActual;
+        int porcentajeMaxActual;
+
+        private void btn_simular_Click(object sender, EventArgs e)
+        {
+            if (txt_simulacion.Text != "" && txt_desde.Text != "")
+            {
+                simulaciones = Convert.ToInt32(txt_simulacion.Text);
+                desde = Convert.ToInt32(txt_desde.Text);
+
+                semanas = 0;
+                dgv_simulaciones.Rows.Clear();
+                //limpiarVariables();
+
+                if (desde < simulaciones)
+                {
+                    hasta = desde + 400; //Consultar si el hasta se ingresa por parametro
+                    txt_hasta.Text = Convert.ToString(desde + 400);
+
+                    for (int i = 0; i < desde; i++)
+                    {
+                        simulacion();
+                    }
+                    cargarGrilla();
+
+
+                    if (hasta > simulaciones)
+                    {
+                        for (int i = 0; i < (simulaciones - desde); i++)
+                        {
+                            simulacion();
+                            cargarGrilla();
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < 400; i++)
+                        {
+                            simulacion();
+                            cargarGrilla();
+                        }
+
+                        for (int i = 0; i < (simulaciones - hasta - 1); i++)
+                        {
+                            simulacion();
+                        }
+
+                        if (hasta != simulaciones)
+                        {
+                            simulacion();
+                            cargarGrilla();
+                        }
+
+                    }
+
+
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Por Favor seleccione un DESDE menor a la cantidad de simulaciones");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por Favor complete todos los campos");
+            }
+        }
+
+
+        public void simulacion()
+        {
+            semanas++;
+            random_demanda = rnd.NextDouble();
+            BuscarDemanda();
+            //random_pedido = rnd.NextDouble();
+
+            /*
+            if (stockRemanenteAnterior >= 0)
+            {
+                BuscarPedido();
+                pedidoNeto = pedido;
+                stockRemanenteActual = stockRemanenteAnterior - demanda + pedidoNeto;
+                stockRemanenteAnterior = stockRemanenteActual;
+
+            }
+            else
+            {
+                BuscarPedido();
+                pedidoNeto = pedido + stockRemanenteAnterior;
+                stockRemanenteActual = pedidoNeto - demanda;
+                stockRemanenteAnterior = stockRemanenteActual;
+                stockReal = 0;
+            }
+            if (stockRemanenteActual < 0)
+            {
+                stockReal = 0;
+            }
+            else
+            {
+                stockReal = stockRemanenteActual;
+            }
+
+            if (stockReal <= 20000)
+            {
+                costoMantenimiento = stockReal * 2500; // el costo es de 2500 por m2 y el stock no supera los 20000
+                costoSobrepaso = 0;
+                excedenteStock = 0;
+            }
+            else
+            {
+                costoMantenimiento = 20000 * 2500; // el costo es de 2500 por m2 y el stock supera los 20000
+                excedenteStock = (stockReal - 20000);
+                costoSobrepaso = excedenteStock * 5000; // el costo de sobrepaso es de 5000 por m2 que se exceda el stock de 20000.
+            }
+
+            costoPedido = 25500;
+            costoTotal = costoMantenimiento + costoPedido + costoSobrepaso;
+            porcentajeCosto = costoTotal / semanas;
+
+
+            if (porcentajeCosto > porcentajeMaxActual)
+            {
+                porcentajeMaxActual = porcentajeCosto;
+            }
+            if (semanas > 1)
+            {
+                if (porcentajeCosto < porcentajeMinActual)
+                {
+                    porcentajeMinActual = porcentajeCosto;
+                }
+            }
+            else
+            {
+                porcentajeMinActual = porcentajeCosto;
+            }*/
+        }
+
+
+        public void BuscarDemanda()
+        {
+            if (random_demanda < 0.5)
+            {
+                demanda = 0;
+            }
+            else
+            {
+                if (random_demanda < 0.65)
+                {
+                    demanda = 1;
+                }
+                else
+                {
+                    if (random_demanda < 0.9)
+                    {
+                        demanda = 2;
+                    }
+                    else
+                    {
+                        if (random_demanda < 1)
+                        {
+                            demanda = 3;
+                        }
+                        
+                    }
+                }
+            }
+
+            //return demanda;
+        }
+
+        public void cargarGrilla()
+        {
+            dgv_simulaciones.Rows.Add(Convert.ToString(semanas), Math.Round(random_demanda, 4),
+                demanda); /*, Math.Round(random_pedido, 4), pedido, pedidoNeto, stockRemanenteActual, stockReal,
+                excedenteStock, costoMantenimiento, costoSobrepaso, costoPedido, costoTotal,
+                porcentajeCosto, porcentajeMinActual, porcentajeMaxActual);*/
+        }
+
+    }
+}
